742758aea06fe78e2233e1b8ecb4a675e0f25515
weix <wechasing@163.com>
2015-09-27 22:42:22 +0800

    init the fabric.env

--- executor.py
+++ executor.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+
 """
     The entrance of the framework
 """
 
+import os
+import xml.etree.cElementTree as ET
+import fabric
+
+def initEnv():
+    """
+        init the environment
+    """
+    filePath = "./conf/conf.xml"
+    tree = ET.ElementTree(file=filePath)
+    root = tree.getroot()
+    configuration = root.find('configuration')
+    roles = configuration.find('roles')
+    pass
+
 
 def executor():
+    initEnv()
+
     pass
 
 
