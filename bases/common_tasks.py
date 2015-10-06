6a2a43177e7e9d00fd073349c3d1652e116d7c80
weix <wechasing@163.com>
2015-10-06 23:35:51 +0800

    add some base tasks such as cope_file execute_command

--- bases/common_tasks.py
+++ bases/common_tasks.py
@@ -3,3 +3,34 @@
 """
     some basic tasks in our common operations
 """
+
+
+from fabric.api import task
+
+@task
+def copy_file(path):
+    """
+    copying the file (file and folder)
+    :param path: the path of the file or folder
+    :return:
+    """
+
+    pass
+
+@task
+def execute_command(command):
+    """
+    executing the specify command
+    :param command: the command to execute
+    :return:
+    """
+
+    if type(command) == list:
+        pass
+    elif isinstance(command,basestring):
+        pass
+    else:
+        pass
+
+
+
